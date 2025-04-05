2b7cc1a89f094d109c1774107ad59b539b2db0c5
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2025-04-05 17:56:53 +1100

    Set logo, click thru and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/thesestickers-logo.png';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="logo">These Stickers</h1>
+      <Link to="/" className="logo-link">
+        <img src={logo} alt="These Stickers Logo" className="logo-img" />
+      </Link>
       <ul>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/shop-policies">Shop Policies</Link></li>
@@ -16,3 +19,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
