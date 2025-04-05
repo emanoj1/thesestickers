11c057d98cb5210611ced1acee30e790c11b97a9
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2025-04-05 18:29:20 +1100

    Insert Instagram and Tally Forms link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,11 @@ function Contact() {
   return (
     <div className="page">
       <h2>Contact</h2>
-      <p>You can reach me via Instagram (@thesestickers) or fill out this form.</p>
+      <p>You can reach me via Instagram <a href="https://www.instagram.com/thesestickers/" target="_blank" rel="noopener noreferrer">(@thesestickers) </a> 
+        <strong>
+            or <a href="https://tally.so/r/w8qkjP" target="_blank" rel="noopener noreferrer">fill out this form</a>.
+        </strong>
+    </p>
     </div>
   );
 }
